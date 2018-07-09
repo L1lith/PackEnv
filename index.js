b9459a010e3843db41080eb249275cd2e56143ad
L1lith <lily@lillith.pw>
2018-07-08 20:38:05 -0600

    add warnings

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ process.env.NODE_ENV = process.env.NODE_ENV || "development"
 const environment = require(`./webpack.${process.env.NODE_ENV}.js`)
 
 
-function packEnvironments(configuration) {
+function packEnvironments(configuration, warnings=true) {
   const environment = process.env.NODE_ENV
   if (typeof environment !== 'string') throw new Error('Node Missing Environment')
   if (environment === 'common') throw new Error('Node Environment cannot be "common"')
-  if (!configuration.hasOwnProperty(environment)) throw new Error(`No configuration for environment "${environment}"`)
   if (typeof configuration != 'object' || configuration === null) throw new Error('Configuration type invalid')
-  Object.entries(configuration).forEach(([key, value]) => {
+  const entries = Object.entries(configuration)
+
+  if (entries.length < 1 && warnings === true) {
+    console.warn('No configuration options!')
+  }
+
+  entries.forEach(([key, value]) => {
     if (typeof value != 'object' || value === null) throw new Error(`Configuration option "${key}"`)
   })
 
@@ -20,7 +25,11 @@ function packEnvironments(configuration) {
   if (configuration.hasOwnProperty('common')) {
     outputConfiguration = configuration.common
   }
-  outputConfiguration = merge(outputConfiguration, configuration[environment])
+  if (configuration.hasOwnProperty(environment)) {
+    outputConfiguration = merge(outputConfiguration, configuration[environment])
+  } else if (warnings === true) {
+    console.warn('There is no configuration for your environment.')
+  }
   return outputConfiguration
 }
 
