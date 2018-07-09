faf15d11e3faf6f0b2e679b029bc5b010bc5eca9
L1lith <lily@lillith.pw>
2018-07-08 20:21:16 -0600

    fix bug and add dependency

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || "development"
 const environment = require(`./webpack.${process.env.NODE_ENV}.js`)
 
 
-function packEnvironments(configurations) {
+function packEnvironments(configuration) {
   const environment = process.env.NODE_ENV
   if (typeof environment !== 'string') throw new Error('Node Missing Environment')
   if (environment === 'common') throw new Error('Node Environment cannot be "common"')
   if (!configuration.hasOwnProperty(environment)) throw new Error(`No configuration for environment "${environment}"`)
-  if (typeof configuration != 'object' || configuration !== null) throw new Error('Configuration type invalid')
+  if (typeof configuration != 'object' || configuration === null) throw new Error('Configuration type invalid')
   Object.entries(configuration).forEach(([key, value]) => {
     if (typeof value != 'object' || value === null) throw new Error(`Configuration option "${key}"`)
   })
